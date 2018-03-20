82e67226cc4b5f57d45acbdd84177b9c689dbe25
onyoukang <31480088+onyoukang@users.noreply.github.com>
2018-03-20 03:43:06 -0500

    Add files via upload

---
+++ microsite/js/javascript.js
@@ -0,0 +1,15 @@
+
+jQuery(document).ready(function($){
+ 
+  //detect if we support scroll snap or not (not the best but should work)
+  var scrollSnap = false;
+  scrollSnap = 'webkitScrollSnapType' in document.documentElement.style;
+  scrollSnap = 'scrollSnapType' in document.documentElement.style;
+  
+  if(scrollSnap === true){
+      $('#supports').append('<h3 class="success">It looks like your browser does support scroll snap!</h3>');
+  }else{
+      $('#supports').append('<h3 class="failure">It looks like your browser might not support scroll snap properties</h3>');
+  }
+});
+
